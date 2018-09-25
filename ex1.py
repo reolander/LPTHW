76988ddc0d1e252497f0ae9af7b501b6ee4e905c
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-09-25 12:33:01 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print "I'd much rather you 'not'."
+#print 'Yay! Printing.'
+# print 'I "said" do not touch this.'
+print "How the what the why the 'lol'."
