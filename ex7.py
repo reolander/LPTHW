d7a809dcab93d3a5722d0c22a6ba9fc8b50e33d1
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-09-28 12:19:22 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print "Mary had a little lamb." # print this string
+print "Its fleece was white as %s." % 'snow' # a string literal embedded in a string literal using string formatting.
+print "." * 10 # what'd that do? Printed 10 dots, hmm let me try with subtraction and division. 
+# For addition Sub and also div it is unssuported operator str and int. Only * is supported.
+
+end1 = "C" # storing a character in a variable.
+end2 = "h" # storing a character in a variable.
+end3 = "e" # storing a character in a variable.
+end4 = "e" # storing a character in a variable.
+end5 = "s" # storing a character in a variable.
+end6 = "e" # storing a character in a variable.
+end7 = "B" # storing a character in a varibale. ALso one need to realize that variable names can't with with numericals but can contain them.
+end8 = "u" # storing a character in a variable.
+end9 = "r" # storing a character in a variable.
+end10 = "g" # storing a character in a variable.
+end11 = "e" # storing a character in a variable.
+end12 = "r" # storing a character in a variable. 
+
+# watch that comma at the end. Try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # comma makes sure that both 'prints' happen in the same line. Else different lines.
+print end7 + end8 + end9 + end10 + end11 + end12
