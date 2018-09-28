852c7d9c5c03f0fd35682772c37784cdf6e36eb7
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-09-28 12:19:56 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun" # Variable days stores the above string 
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # \n representes a new line so Jan would in line 1 and Feb inline 2...
+
+
+# print "Here are the days: %s" %days
+# print "Here are the days: ", days, months
+print "Here are the days: ", days # You can use formatting to embed a variable/literal in a string or not.
+print "Here are the months: ", months #print months.
+
+# use three double quotes to print a paragraph. As more than 80 characters per line is bad code in Python.
+print """ 
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 5 lines if we want, or 5, or 6. 
+"""
