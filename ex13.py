bb35094881b8aad25cb8522c40a812ab8c48d867
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-10-02 14:10:49 +0530

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+#script, first, second, third, = argv
+
+#print "The script is called:", script
+#print "Your first variable is:", first
+#print "Your second variable is:", second
+#print "Your third variable is:", third 
+
+# writing code which can take 3 command line arguments
+
+script, first_arg, second_arg = argv
+
+random_input = raw_input("Enter somethig bro: ")
+
+print "The script is called:", script
+print "Your first variable is:", first_arg
+print "Your second variable is:", second_arg
+
+print "Your input from raw_input function is:", random_input
+
+
+
+# Writing code where argv contains 5 values.
+
+#script, first_arg, second_arg, third_arg, fourth_arg = argv
+
+#print "The script is called:", script
+#print "Your first variable is:", first_arg
+#print "Your second variable is:", second_arg
+#print "Your third variable is:", third_arg
+#print "Your fourth variable is:", fourth_arg
