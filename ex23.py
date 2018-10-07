1ba61189376a1301ce6edd5f48b2c51ac4eaa159
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-10-07 17:01:17 +0530

    Add files via upload

---
+++ ex23.py
@@ -0,0 +1,7 @@
+'''
+Should go to Github and checkout a python repository and should 
+try to read a python script file. The goal is to:
+	1. Have the ability to find the source code that I'm looking for.
+	2. Ability to read code and start looking for files.
+	3. Understand the code in the files that I find.
+'''
