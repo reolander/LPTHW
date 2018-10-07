6de019284b9f9bbfbbe8f39ff381ba818c7a9677
Rohan Mohammad <rohan.mohammad@protonmail.com>
2018-10-07 17:00:57 +0530

    Add files via upload

---
+++ ex22.py
@@ -0,0 +1,5 @@
+'''
+I just need to know the english names for all the symbols on my keyboard
+and also I should know what they do in programming context. 
+For example Asterisk - * - Multiplication.
+'''
